17590dceeaaffd4132b34ee13e8f738bb78d4f24
Rushi Gandhi <rushi.gandhi@hotmail.com>
2019-05-18 01:04:36 -0400

    The server is now able to connect to clients

--- DensityIntegrate/device.js
+++ DensityIntegrate/device.js
@@ -1,5 +1,19 @@
+// imports
 const net = require('net');
+
+// config
 const PORT = "9000";
 const HOST = 'localhost';
 
+// server instance
 const server = net.createServer();
+
+// when a client connects
+server.on("connection", function(socket){
+
+});
+
+// when there is an error connecting
+server.on("error", function(){
+    console.log("Some error has occurred...");
+});
