648a188c79d2551b3d500240388c8810ec93dc83
Rushi Gandhi <rushi.gandhi@hotmail.com>
2019-05-18 01:08:22 -0400

    Server listens and is able to pull data

--- DensityIntegrate/device.js
+++ DensityIntegrate/device.js
@@ -10,10 +10,17 @@ const server = net.createServer();
 
 // when a client connects
 server.on("connection", function(socket){
+    var remoteAddress = socket.remoteAddress + ":" + socket.remotePort;
+    console.log("New client connection is made %s", remoteAddress);
 
 });
 
 // when there is an error connecting
 server.on("error", function(){
     console.log("Some error has occurred...");
+});
+
+// server starts listening on specified port
+server.listen(9000, function(){
+    console.log("Server listening to %j", server.address().port);
 });
