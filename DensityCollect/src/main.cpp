cfadad043f0820d9cc907797a413e9ceb5ad442d
Shahbaz Momi <shahbaz.man3@gmail.com>
2019-05-18 01:23:34 -0400

    setup required methods and completed wifi parser package

--- DensityCollect/src/main.cpp
+++ DensityCollect/src/main.cpp
@@ -20,6 +20,44 @@ extern "C"
 MacCache cache;
 int channel = 1;
 
+// According to the SDK documentation, the packet type can be inferred from the
+// size of the buffer. We are ignoring this information and parsing the type-subtype
+// from the packet header itself. Still, this is here for reference.
+wifi_promiscuous_pkt_type_t packet_type_parser(uint16_t len)
+{
+    switch(len)
+    {
+        // If only rx_ctrl is returned, this is an unsupported packet
+        case sizeof(wifi_pkt_rx_ctrl_t):
+            return WIFI_PKT_MISC;
+
+            // Management packet
+        case sizeof(wifi_pkt_mgmt_t):
+            return WIFI_PKT_MGMT;
+
+            // Data packet
+        default:
+            return WIFI_PKT_DATA;
+    }
+}
+
+void wifi_sniffer_packet_handler(uint8_t *buff, uint16_t len)
+{
+
+}
+
+void dump_serial() {
+
+    }
+
+Serial.print(FRAME_END);
+}
+
+void setup()
+{
+
+}
+
 void loop()
 {
     if(last_exec_time == 0) {
