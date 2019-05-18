db262a8a5211a4691a8f94c8e1ae557312da157f
Shahbaz Momi <shahbaz.man3@gmail.com>
2019-05-18 01:48:43 -0400

    added wifi sniffer packet handler

--- DensityCollect/src/main.cpp
+++ DensityCollect/src/main.cpp
@@ -43,7 +43,79 @@ wifi_promiscuous_pkt_type_t packet_type_parser(uint16_t len)
 
 void wifi_sniffer_packet_handler(uint8_t *buff, uint16_t len)
 {
+// First layer: type cast the received buffer into our generic SDK structure
+    const wifi_promiscuous_pkt_t *ppkt = (wifi_promiscuous_pkt_t *)buff;
+    // Second layer: define pointer to where the actual 802.11 packet is within the structure
+    const wifi_ieee80211_packet_t *ipkt = (wifi_ieee80211_packet_t *)ppkt->payload;
+    // Third layer: define pointers to the 802.11 packet header and payload
+    const wifi_ieee80211_mac_hdr_t *hdr = &ipkt->hdr;
+    // const uint8_t *data = ipkt->payload;
 
+    // Pointer to the frame control section within the packet header
+    const wifi_header_frame_control_t *frame_ctrl = (wifi_header_frame_control_t *)&hdr->frame_ctrl;
+
+    // store the addresses in the cache
+    // NOTE: always use the sender address
+    // for proper rssi receival
+    if(frame_ctrl->to_ds == 0 && frame_ctrl->from_ds == 0) { // 00
+        // addr2 == SA
+        cache.occurrence(hdr->addr2, ppkt->rx_ctrl.rssi);
+    } else if(frame_ctrl->to_ds == 1 && frame_ctrl->from_ds == 0) { // 10
+        // addr2 == SA
+        cache.occurrence(hdr->addr2, ppkt->rx_ctrl.rssi);
+    } else if(frame_ctrl->to_ds == 0 && frame_ctrl->from_ds == 1) { // 01
+        // addr3 == SA
+        cache.occurrence(hdr->addr3, ppkt->rx_ctrl.rssi);
+    } else { // 11
+        // never
+    }
+
+    // Parse MAC addresses contained in packet header into human-readable strings
+    // char addr1[] = "00:00:00:00:00:00\0";
+    // char addr2[] = "00:00:00:00:00:00\0";
+    // char addr3[] = "00:00:00:00:00:00\0";
+
+    // mac2str(hdr->addr1, addr1);
+    // mac2str(hdr->addr2, addr2);
+    // mac2str(hdr->addr3, addr3);
+
+    // // Output info to serial
+    // Serial.printf("\n%s | %s | %s | %u | %02d | %u | %u(%-2u) | %-28s | %u | %u | %u | %u | %u | %u | %u | %u | ",
+    //   addr1,
+    //   addr2,
+    //   addr3,
+    //   wifi_get_channel(),
+    //   ppkt->rx_ctrl.rssi,
+    //   frame_ctrl->protocol,
+    //   frame_ctrl->type,
+    //   frame_ctrl->subtype,
+    //   wifi_pkt_type2str((wifi_promiscuous_pkt_type_t)frame_ctrl->type, (wifi_mgmt_subtypes_t)frame_ctrl->subtype),
+    //   frame_ctrl->to_ds,
+    //   frame_ctrl->from_ds,
+    //   frame_ctrl->more_frag,
+    //   frame_ctrl->retry,
+    //   frame_ctrl->pwr_mgmt,
+    //   frame_ctrl->more_data,
+    //   frame_ctrl->wep,
+    //   frame_ctrl->strict);
+
+    // // Print ESSID if beacon
+    // if (frame_ctrl->type == WIFI_PKT_MGMT && frame_ctrl->subtype == BEACON)
+    // {
+    //   const wifi_mgmt_beacon_t *beacon_frame = (wifi_mgmt_beacon_t*) ipkt->payload;
+    //   char ssid[32] = {0};
+
+    //   if (beacon_frame->tag_length >= 32)
+    //   {
+    //     strncpy(ssid, beacon_frame->ssid, 31);
+    //   }
+    //   else
+    //   {
+    //     strncpy(ssid, beacon_frame->ssid, beacon_frame->tag_length);
+    //   }
+
+    //   Serial.printf("%s", ssid);
+    // }
 }
 
 void dump_serial() {
